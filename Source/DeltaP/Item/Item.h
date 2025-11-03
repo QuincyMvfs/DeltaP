5aa62e8d9ec0e18f1853efd944032f1e57170820
Navpreet Singh <singh.navpreet14.1@gmail.com>
2025-11-03 12:37:28 -0800

    created item in cpp

---
+++ Source/DeltaP/Item/Item.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class DELTAP_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+
+protected:
+	virtual void BeginPlay() override;
+};
