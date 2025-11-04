1c3e8e7f6b3facb1c8246cfb0ddc752a4abd5248
Navpreet Singh <singh.navpreet14.1@gmail.com>
2025-11-03 17:22:03 -0800

    Interact setup for UI done.

--- Source/DeltaP/ActorComponents/InteractComponent.cpp
+++ Source/DeltaP/ActorComponents/InteractComponent.cpp
@@ -3,14 +3,77 @@
 
 #include "InteractComponent.h"
 
+#include "DeltaP/Interfaces/Interact.h"
+#include "Kismet/GameplayStatics.h"
+
 UInteractComponent::UInteractComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.TickInterval = 0.1f;
+
+	SetIsReplicated(true);
 }
 
 void UInteractComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	TryUpdatingReferences();
+
+	// Only turn tick on for the client since this would mostly be for UI
+	if (IsValid(OwningActor) && OwningActor->HasLocalNetOwner())
+	{
+		PrimaryComponentTick.bCanEverTick = true;
+		PrimaryComponentTick.TickInterval = 0.1f;
+	}
+}
+
+void UInteractComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!IsValid(OwningActor) && !IsValid(CameraManager))
+	{
+		TryUpdatingReferences();
+	}
+	
+	if (IsActive() && OwningActor->HasLocalNetOwner()) return;
+
+	FVector StartLocation = CameraManager->GetCameraLocation();
+	FVector EndLocation = (CameraManager->GetActorForwardVector() * InteractDistance) + StartLocation;
+
+	FHitResult Hit;
+	UKismetSystemLibrary::CapsuleTraceSingle(this,
+		StartLocation,
+		EndLocation,
+		CapsuleRadius,
+		0.0f,
+		UEngineTypes::ConvertToTraceType(ECC_Visibility),
+		false,
+		{OwningActor},
+		DrawDebugType,
+		Hit, true,
+		FLinearColor::Red,
+		FLinearColor::Green,
+		DebugTraceDuration);
+	
+	if (AActor* HitActor = Hit.GetActor())
+	{
+		if (HitActor->GetClass()->ImplementsInterface(UInteract::StaticClass()))
+		{
+			bool CanInteract = IInteract::Execute_CanInteract(HitActor);
+			if (CanInteract)
+			{
+				OnSuccessfulHit.Broadcast(IInteract::Execute_GetInteractInfo(HitActor));
+			}
+		}
+	}
+}
+
+void UInteractComponent::TryUpdatingReferences()
+{
+	OwningActor = GetOwner();
+	CameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
 }
 
 
--- Source/DeltaP/ActorComponents/InteractComponent.h
+++ Source/DeltaP/ActorComponents/InteractComponent.h
@@ -4,10 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "DeltaP/Interfaces/Interact.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "InteractComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSuccessfulHit, FInteractionInfo, InteractionInfo);
 
-UCLASS(ClassGroup=(Custom), BlueprintType, Blueprintable, meta=(BlueprintSpawnableComponent))
+
+UCLASS(ClassGroup=(Custom), Blueprintable, meta=(BlueprintSpawnableComponent))
 class DELTAP_API UInteractComponent : public UActorComponent
 {
 	GENERATED_BODY()
@@ -17,4 +21,30 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION()
+	void TryUpdatingReferences();
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Interact")
+	AActor* OwningActor;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Interact")
+	APlayerCameraManager* CameraManager;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interact")
+	float InteractDistance = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interact|Trace Settings")
+	float CapsuleRadius = 15.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interact|Trace Settings")
+	TEnumAsByte<EDrawDebugTrace::Type> DrawDebugType = EDrawDebugTrace::None;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interact|Trace Settings", meta=(EditCondition="DrawDebugType == EDrawDebugTrace::ForDuration") )
+	float DebugTraceDuration = 0.0f;
+
+	UPROPERTY(BlueprintAssignable, Category = "Interact")
+	FOnSuccessfulHit OnSuccessfulHit;
 };
