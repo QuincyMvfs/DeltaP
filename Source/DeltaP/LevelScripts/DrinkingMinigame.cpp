b536e07c19f894721747bb0be75946bbc1329e60
Quincy Michels <quincy.michels@gmail.com>
2025-11-17 14:28:27 -0500

    started on drinking minigame

---
+++ Source/DeltaP/LevelScripts/DrinkingMinigame.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DrinkingMinigame.h"
+
+// Sets default values
+ADrinkingMinigame::ADrinkingMinigame()
+{
+
+}
+
+// Called when the game starts or when spawned
+void ADrinkingMinigame::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
